4b68746901ca8c9d386f781bb0be9f7decd17971
rozhkovdmitrii <rozhkov@komodoplatform.com>
2024-05-08 20:35:04 +0500

    renaming

--- transmitter-module/src/watcher_app/solana_logs_processor.rs
+++ transmitter-module/src/watcher_app/solana_logs_processor.rs
@@ -8,7 +8,7 @@ use crate::common::solana_logs::{
     event_processor::EventProcessor, solana_event_listener::LogsBunch,
 };
 
-use transmitter_common::data::OperationExecuted;
+use transmitter_common::data::{OpHash, OperationExecuted};
 
 pub(super) struct OperationExecutedEventProcessor {
     logs_receiver: Mutex<UnboundedReceiver<LogsBunch>>,
@@ -34,14 +34,15 @@ impl OperationExecutedEventProcessor {
 }
 
 impl EventProcessor for OperationExecutedEventProcessor {
-    type Event = photon::OperationExecuted;
+    type Event = photon::ProposalExecuted;
 
     fn on_event(&self, event: Self::Event, signature: &str, _slot: u64) {
         debug!("OperationExecuted status event intercepted: {:?}", event);
 
         if let Err(err) = self.op_status_sender.send(OperationExecuted {
             last_watched_block: signature.to_string(),
-            op_hash: *event.op_hash.first_chunk().expect(""),
+            op_hash: OpHash::try_from(event.op_hash)
+                .expect("op_hash expected to be gotten from proposal_executed event"),
             executor: event.executor,
         }) {
             error!("Failed to send proposal through the channel: {}", err);
