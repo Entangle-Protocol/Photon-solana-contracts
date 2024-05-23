0ce8f95415a3ed7a96ebc29814defbfd6d3eb97d
Dmitry <d.tokarev@entangle.fi>
2024-05-23 17:18:27 +0500

    Add max-sign-attempts flag

--- deploy_mainnet.sh
+++ deploy_mainnet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 solana program deploy \
-    --with-compute-unit-price 10000000 --commitment confirmed \
+    --with-compute-unit-price 10000000 --commitment confirmed --max-sign-attempts 1 \
     --buffer recover.json \
     --program-id ./target/deploy/photon-keypair.json \
     ./target/deploy/photon.so
