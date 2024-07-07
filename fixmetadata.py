4adba09468448919d91507af266080d7f64a9c06
Dmitrii Rozhkov <drozhkov@ent-dx.com>
2024-07-07 14:31:21 +0000

    migrate

--- fixmetadata.py
+++ fixmetadata.py
@@ -9,8 +9,5 @@ def fix(path, address):
     metadata["metadata"]["address"] = address
     open(path, "w").write(json.dumps(metadata))
 
-if len(sys.argv) == 2 and sys.argv[1] == "mainnet":
-    fix(photon, "pccm961CjaR7T7Hcht9omrXQb9w54ntJo95FFT7N9AJ")
-else:
-    fix(photon, "JDxWYX5NrL51oPcYunS7ssmikkqMLcuHn9v4HRnedKHT")
-    fix(onefunc, "EjpcUpcuJV2Mq9vjELMZHhgpvJ4ggoWtUYCTFqw6D9CZ")
+fix(photon, "pccm961CjaR7T7Hcht9omrXQb9w54ntJo95FFT7N9AJ")
+fix(onefunc, "EjpcUpcuJV2Mq9vjELMZHhgpvJ4ggoWtUYCTFqw6D9CZ")
--- transmitter-common/src/lib.rs
+++ transmitter-common/src/lib.rs
@@ -8,8 +8,11 @@ pub mod utils;
 
 extern crate photon;
 
-#[cfg(feature = "devnet")]
+#[cfg(not(any(feature = "devnet", feature = "localnet", feature = "mainnet")))]
+compile_error!("Either feature \"devnet\", \"localnet\" or \"mainnet\" must be defined");
+
+#[cfg(any(feature = "devnet", feature = "localnet"))]
 pub const SOLANA_CHAIN_ID: u128 = 100000000000000000000;
 
-#[cfg(not(feature = "devnet"))]
+#[cfg(feature = "mainnet")]
 pub const SOLANA_CHAIN_ID: u128 = 11100000000000000501;
