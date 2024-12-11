13ce0293c4c7b2f51de1a79874c5b63f50628d02
N. Siplis <nicolas.siplis@gmail.com>
2024-12-11 11:56:57 -0300

    Uncomment test script line.

--- docker/build_solana_test_state.sh
+++ docker/build_solana_test_state.sh
@@ -3,4 +3,4 @@ python3 fixmetadata.py
 solana-test-validator -q --reset --bpf-program keys/onefunc-keypair.json /deploy/onefunc.so \
                                  --bpf-program keys/photon-keypair.json /deploy/photon.so &
 sleep 1
-# anchor test --skip-local-validator --skip-build --skip-deploy
+anchor test --skip-local-validator --skip-build --skip-deploy
